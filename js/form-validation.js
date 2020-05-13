f9994571e1654c633bfff58937de9dec17b6a8bb
tatianaazulay <tania142857@gmail.com>
2020-05-13 03:19:10 -0400

    mask update

--- js/form-validation.js
+++ js/form-validation.js
@@ -23,16 +23,16 @@ $(function() {
             digits: true,
             rangelength: [3, 4]
         },
-       // email: {
-      //    required: false,
-          //email is validated by the built-in "email" rule
-     //    email: true
-      //  },
+       email: {
+          required: false,
+          email: true
+        },
       },
       // Specify validation error messages
       messages: {
         firstname: "Please enter your firstname",
         lastname: "Please enter your lastname",
+        email: "Please enter valid email",
         zip: "Please enter valid zip code",
         CCnumber: "Please enter valid card number",
         CCcvv: "Please enter valid cvv",
@@ -52,40 +52,32 @@ $(function() {
   });
 
 
-  var mytextbox = document.getElementById('CCnumber'),cardNum, displayedNum, cursor;;
-  function myKeyPress(e){
-    mytextbox.addEventListener('input', inputChanged);
+  function myKeyPress(event){
+    var cardNum = document.getElementById('CCnumber').value;
+    console.log(cardNum);
+    var keyPressed="";
+    if(window.event && event.keyCode>=48 && event.keyCode<=57) { 
+      keyPressed = event.keyCode;
+    } else if(event.which  && event.keyCode>=48 && event.keyCode<=57){ 
+      keyPressed = event.which;
+    }
+
+if (keyPressed !==""){
+  var num = String.fromCharCode(keyPressed);
+  var maskedNum;
+  maskedNum = maskNum(cardNum);
+  console.log("Key Pressed = " + keyPressed);
+  console.log("  Formatted = " + maskedNum);
+  if(cardNum.length >= 19){
+    return false
+}
+document.getElementById('CCnumber').value = maskedNum;
+}else
+return false;
+
+  };  
+  function maskNum(value){
+    if(value.length == 4 || value.length == 9 || value.length == 14 )
+      value = value + ' ';
+    return value;
   };
-  function inputChanged(e){
-      displayedNum = e.target;
-      cardNum = e.target.value;
-      cursor = e.target.selectionEnd;//Cursor specifies the index of the character after the selection(current input)
-      console.log(cursor);//This displays the index of the character after the selection       
-      cardNum = e.target.value;
-      console.log("the value from the text input: "+cardNum);  
-      cardNum = cardNum.replace(new RegExp(/[^\d]/g), ''); // Remove every non-digit character 
-      console.log("Key Pressed (real phone number)= "+cardNum);//displays UNFORMATTED cardNum, which ignores all non-digit characters
-      if(cardNum.match(/^[0-9]*$/g)) {
-        cardNum = formatCardNumber(cardNum);
-        console.log("Formatted = "+cardNum); //displays FORMATTED cardNum
-      if(cardNum !== "") {
-        displayedNum.value = cardNum;
-      } else {
-        displayedNum.value = "";
-      }
-      }
-    };
-    function formatCardNumber(value) {
-      if(value.length > 4 && value.length <= 8 ){
-        value = value.slice(0,4) + " " + value.slice(4);
-        newCursor=cursor++;
-      } if(value.length > 8 && value.length <= 12){
-        value =value.slice(0,4) + " " + value.slice(4,8)+ " " + value.slice(8);
-        newCursor=cursor+2;
-        }
-        else if(value.length > 12 ){
-            value =value.slice(0,4) + " " + value.slice(4,8)+ " " + value.slice(8,12) + " " + value.slice(12);
-            newCursor=cursor+2;
-            }
-      return value;
-    };
